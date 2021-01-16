dd40271aabfd12c62b6feafc41eb2baadc121546
Freely Kathryne <fkathryn@si-g3.kzn.21-school.ru>
2021-01-16 12:53:22 +0300

    stack with std::vector and list, vector prototypes

--- Stack.hpp
+++ Stack.hpp
@@ -2,7 +2,47 @@
 // Created by Freely Kathryne on 1/15/21.
 //
 
-#ifndef FT_CONTAINERS_STACK_HPP
-#define FT_CONTAINERS_STACK_HPP
+#ifndef STACK_HPP
+#define STACK_HPP
 
-#endif //FT_CONTAINERS_STACK_HPP
+#include <iostream>
+#include <vector>
+
+namespace ft {
+	template<class T, class Container = std::vector<T> >
+	class stack {
+		typedef T value_type;
+		typedef Container container_type;
+		typedef size_t size_type;
+
+	public:
+
+		//member function:
+		bool empty() const { return this->_container.empty(); }
+		size_type size() const { return this->_container.size(); }
+		value_type & top() { return this->_container.back(); };
+		const value_type & top() const { return this->_container.push_back(); };
+		void push(const value_type& val) { return this->_container.push_back(val); };
+		void pop() { this->_container.pop_back(); };
+
+
+		explicit stack(const container_type & container = container_type()) { this->_container = container; };
+
+	private:
+		container_type _container;
+	};
+	template <class T, class Container>
+		bool operator==(const stack<T,Container>& lhs, const stack<T,Container>& rhs);
+	template <class T, class Container>
+		bool operator!=(const stack<T,Container>& lhs, const stack<T,Container>& rhs);
+	template <class T, class Container>
+		bool operator<(const stack<T,Container>& lhs, const stack<T,Container>& rhs);
+	template <class T, class Container>
+		bool operator<=(const stack<T,Container>& lhs, const stack<T,Container>& rhs);
+	template <class T, class Container>
+		bool operator>(const stack<T,Container>& lhs, const stack<T,Container>& rhs);
+	template <class T, class Container>
+		bool operator>=(const stack<T,Container>& lhs, const stack<T,Container>& rhs);
+}
+
+#endif //STACK_HPP
