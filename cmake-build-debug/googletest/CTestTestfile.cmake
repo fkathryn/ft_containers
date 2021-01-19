c38413548ee8475e51ba2b1216269f1c9bc8c651
Freely Kathryne <fkathryn@un-h4.kzn.21-school.ru>
2021-01-20 02:47:54 +0300

    recovery cmake and google test

---
+++ cmake-build-debug/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/fkathryn/Desktop/ft_containers/googletest
+# Build directory: /Users/fkathryn/Desktop/ft_containers/cmake-build-debug/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
